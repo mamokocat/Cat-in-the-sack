019f578e642c972ecd216eb56357c716def327d3
Artem Kivachuk <mamokosf@gmail.com>
2019-07-30 17:31:26 +0300

    Relations between pages improved (new buttons)

--- Cat_in_the_sack/Scripts/code.js
+++ Cat_in_the_sack/Scripts/code.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $(".movie").hover( function(){
         $(this).css({
             cursor: 'pointer',
-            background: '#2c2c2c',
             transform: 'scale(1.2)',
             transitionDuration: '0.3s'
         })
